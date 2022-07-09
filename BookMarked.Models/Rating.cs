93d8d31414b9468ca844ccbb48fd6576a0e739c3
mercedezbray <mercedezbray@gmail.com>
2022-07-09 13:52:30 -0400

    updated models with data annotations

--- BookMarked.Models/Rating.cs
+++ BookMarked.Models/Rating.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,11 @@ namespace BookMarked.Models
     {
         public int RatingId { get; set; }
         public int ReviewId { get; set; }
+        [Required(ErrorMessage ="Rating must be a whole number between 1-10")]
+        [Range(1,10)]
         public int Stars { get; set; }
+        [Required(ErrorMessage ="Must enter a date in format MM/DD/YYYY")]
+        [Display(Name ="Date Read")]
         public DateTime DateRead { get; set; }
     }
 }
--- BookMarked.Models/Review.cs
+++ BookMarked.Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace BookMarked.Models
     {
         public int ReviewId { get; set; }
         public int RatingId { get; set; }
+        [Required (ErrorMessage ="Must enter text for a review")]
+        [Display(Name="Review")]
+        [StringLength(1000)]
         public string ReviewContent { get; set; }
     }
 }
