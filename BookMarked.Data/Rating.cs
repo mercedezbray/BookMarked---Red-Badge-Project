fa5d6b0305100f993ccd6e36c44395dbed3c2260
mercedezbray <mercedezbray@gmail.com>
2022-07-09 14:41:26 -0400

    entities made

---
+++ BookMarked.Data/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMarked.Data
+{
+    public class Rating
+    {
+        [Key]
+        public int RatingId { get; set; }
+        [Required]
+        public int ReviewId { get; set; }
+        [Required]
+        public int Stars { get; set; }
+        [Required]
+        public DateTime DateRead { get; set; }
+    }
+}
---
+++ BookMarked.Data/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMarked.Data
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        [Required]
+        public int RatingId { get; set; }
+        [Required]
+        public string ReviewContent { get; set; }
+    }
+}
