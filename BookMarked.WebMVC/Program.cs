0881e89cf62ccc226580e88c1adc2e226b9b41de
mercedezbray <mercedezbray@gmail.com>
2022-07-09 11:56:05 -0400

    added api key to program.cs

--- BookMarked.WebMVC/Program.cs
+++ BookMarked.WebMVC/Program.cs
@@ -1,4 +1,6 @@
 using BookMarked.Data;
+using Google.Apis.Books.v1;
+using Google.Apis.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,13 +8,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+//builder.Services.AddScoped<INoteService, NoteService>();
+//builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+builder.Services.AddSingleton(options =>
+    new BooksService(new BaseClientService.Initializer { 
+        ApiKey = builder.Configuration["GoogleBooksAPIKey"],
+        ApplicationName = "BookMarked"
+    }));
 
 var app = builder.Build();
 
